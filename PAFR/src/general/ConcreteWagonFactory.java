34a1d6b9d8b823c22944a6122b96aa0642bf1bac
Almar <almar456@hotmail.com>
2019-11-27 12:56:38 +0100

    x

--- PAFR/src/general/ConcreteWagonFactory.java
+++ PAFR/src/general/ConcreteWagonFactory.java
@@ -2,4 +2,10 @@ package general;
 
 public class ConcreteWagonFactory implements WagonFactory{
 
+	@Override
+	public Wagon getWagon() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
